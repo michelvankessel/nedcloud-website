6880fb426af12132d607eb30771d2784080018d3
michelvankessel <michelvankessel@protonmail.com>
2026-06-17 11:27:28 +0200

    feat: Configure allowed development origins in Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
+  allowedDevOrigins: ['192.168.0.204', 'www.nedcloudsolutions.nl', 'nedcloudsolutions.nl'],
 
   images: {
     remotePatterns: [
